c6f0fa79308180b898dac7ef8bb04cb993ee4fca
kostas <akwstas1@gmail.com>
2022-11-24 12:51:35 +0200

    Add gh-pages dependancy.

--- src/components/buttons/Like/Like.js
+++ src/components/buttons/Like/Like.js
@@ -3,12 +3,18 @@ import '../buttonsCommonStyles.css'
 import './Like.css'
 
 class Like extends Component {
+  componentDidMount() {
+    document.querySelector('.like').addEventListener('click', () => this.props.onclick('like'))
+  }
+  componentWillUnmount() {
+    document.querySelector('.like').removeEventListener('click', () => this.props.onclick('like'))
+  }
   render() {
     return (
       <span
-        onClick={() => {
-          this.props.onclick('like')
-        }}
+        // onClick={() => {
+        //   this.props.onclick('like')
+        // }}
         className="material-symbols-outlined like">
         thumb_up
       </span>
